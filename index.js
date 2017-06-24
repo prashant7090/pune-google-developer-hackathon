4d3f923cdd11bb46cf72cca408eceb385b9f1c61
Prashant <prashant7090@gmail.com>
2017-06-24 13:03:58 +0530

    Added serverless function.

--- index.js
+++ index.js
@@ -69,3 +69,17 @@ exports.helloPubSubAttributes = functions.pubsub.topic('yet-another-topic-name')
   // Print the message in the logs.
   console.log(`Hello ${name || 'World'}!`);
 });
+
+
+// Take the text parameter passed to this HTTP endpoint and insert it into the
+// Realtime Database under the path /messages/:pushId/original
+exports.addMessage = functions.https.onRequest((req, res) => {
+  // Grab the text parameter.
+  const original = req.query.text;
+  // Push the new message into the Realtime Database using the Firebase Admin SDK.
+  admin.database().ref('/messages').push({original: original}).then(snapshot => {
+    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+    res.redirect(303, snapshot.ref);
+  });
+});
+
